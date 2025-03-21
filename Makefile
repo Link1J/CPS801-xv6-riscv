99c555bc096506cb2184cf9e694108de382a95d8
Ihtishamul Haque <ihtishamul.haque@torontomu.ca>
2025-03-20 22:54:23 -0400

    added process_test to test round robin schedule

--- Makefile
+++ Makefile
@@ -133,6 +133,7 @@ UPROGS=\
 	$U/_ln\
 	$U/_ls\
 	$U/_mkdir\
+	$U/_process_test\
 	$U/_rm\
 	$U/_sh\
 	$U/_stressfs\
---
+++ user/process_test.c
@@ -0,0 +1,34 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void run_child(const char *name, int loops) {
+    int start_time = uptime(); // Record start time.
+
+    for (int i = 0; i < loops; i++) {
+        fprintf(2, "%s (PID: %d) running...\n", name, getpid());
+        for (volatile int j = 0; j < 100000000; j++);
+    }
+
+    int end_time = uptime(); // Record end time.
+    int completion_time = end_time - start_time; // Calculate process duration.
+
+    fprintf(2, "%s (PID: %d) exiting | Completion Time: %d ticks\n", name, getpid(), completion_time);
+    exit(0);
+}
+
+int main() {
+    if (fork() == 0) run_child("Child 0", 5);
+    if (fork() == 0) run_child("Child 1", 5);
+    if (fork() == 0) run_child("Child 2", 5);
+    if (fork() == 0) run_child("Child 3", 5);
+    if (fork() == 0) run_child("Child 4", 5);
+    if (fork() == 0) run_child("Child 5", 5);
+    if (fork() == 0) run_child("Child 6", 5);
+    if (fork() == 0) run_child("Child 7", 5);
+
+    while (wait(0) > 0);
+
+    fprintf(2, "--- Round-Robin Test Completed ---\n");
+    exit(0);
+}
